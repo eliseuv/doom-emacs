075a49761b8ba3486636b77837e2ba5c487794c6
evf <eliseuv816@gmail.com>
2025-05-07 15:50:49 -0300

    Separate DOOM Emacs config from NixOS config

---
+++ custom.el
@@ -0,0 +1,29 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(conda-anaconda-home "/opt/anaconda")
+ '(custom-safe-themes
+   '("8ccbbbf5c197f80c9dce116408a248fde1ac4fedd9b5b7883e729eba83c9c64e" "02f57ef0a20b7f61adce51445b68b2a7e832648ce2e7efb19d217b6454c1b644" "8b6506330d63e7bc5fb940e7c177a010842ecdda6e1d1941ac5a81b13191020e" "c865644bfc16c7a43e847828139b74d1117a6077a845d16e71da38c8413a5aaa" "60ada0ff6b91687f1a04cc17ad04119e59a7542644c7c59fc135909499400ab8" "234dbb732ef054b109a9e5ee5b499632c63cc24f7c2383a849815dacc1727cb6" default))
+ '(org-agenda-files
+   '("/home/evf/programs/phd/papers/brass_ca_magnet_ts/notes.org" "/home/evf/Documents/org/agenda.org" "/home/evf/Documents/org/elfeed.org" "/home/evf/Documents/org/masters.org" "/home/evf/Documents/org/notes.org" "/home/evf/Documents/org/phd.org" "/home/evf/Documents/org/reading_list.org" "/home/evf/Documents/org/todo.org"))
+ '(package-selected-packages '(ledger-import gnu-apl-mode latex-preview-pane irony))
+ '(warning-suppress-types
+   '((org-element-cache)
+     (org-element-cache)
+     (org-element-cache)
+     (org-element-cache)
+     (org-element-cache)
+     (org-element-cache)
+     (org-element-cache)
+     (org-element-cache)
+     (org-element-cache)
+     (org-element-cache)
+     (defvaralias))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ts-fold-replacement-face ((t (:foreground nil :box nil :inherit font-lock-comment-face :weight light)))))
